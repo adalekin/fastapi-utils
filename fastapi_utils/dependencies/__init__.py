f24565c37844e4df362ab8da9d130a9b270cc413
Aleksey Dalekin <adalekin@gmail.com>
2022-04-29 00:20:35 +0300

    Rename timezone dependency to get_timezone

--- fastapi_utils/dependencies/__init__.py
+++ fastapi_utils/dependencies/__init__.py
@@ -1,1 +1,2 @@
 from .auth import ensure_current_user, ensure_current_superuser
+from .timezone import get_timezone
--- fastapi_utils/dependencies/timezone.py
+++ fastapi_utils/dependencies/timezone.py
@@ -2,7 +2,7 @@ import timezones
 from fastapi import Header
 
 
-def timezone(x_timezone: str = Header(None, description="Timezone")):
+def get_timezone(x_timezone: str = Header(None, description="Respect the specified timezone")):
     tz = x_timezone or timezones.get_default_timezone_name()
 
     try:
